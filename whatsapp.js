d4ae826c381dcaf09e7fbb7e7f5af9459277205e
Franco Vedia <francoaresis@gmail.com>
2023-11-19 12:12:51 -0300

    border chat added

--- whatsapp.js
+++ whatsapp.js
@@ -711,8 +711,31 @@ let isUp = null;
 let bodyDate = null;
 const setMargin = (e) => {
   if (!isUp) return "mt-1";
-  if (isUp === e) return "mt-1";
-  if (isUp !== e) return "mt-4";
+  if (isUp === e.name) return "mt-1";
+  if (isUp !== e.name) return "mt-4";
+};
+
+const setSvgChat = (e) => {
+  //if (!isUp) return "";
+  if (e.name !== isUp) {
+    if (e.name === "Mindqube") {
+      return `<svg class="absolute right-[8px] text-[#005c4b]" viewBox="0 0 8 13" height="13" width="8" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 8 13"><path opacity="0.13" d="M5.188,1H0v11.193l6.467-8.625 C7.526,2.156,6.958,1,5.188,1z"></path><path fill="currentColor" d="M5.188,0H0v11.193l6.467-8.625C7.526,1.156,6.958,0,5.188,0z"></path></svg>`;
+    } else {
+      return `<svg class="absolute left-[8px] text-[#202c33]" viewBox="0 0 8 13" height="13" width="8" preserveAspectRatio="xMidYMid meet" class="" version="1.1" x="0px" y="0px" enable-background="new 0 0 8 13"><path opacity="0.13" fill="#0000000" d="M1.533,3.568L8,12.193V1H2.812 C1.042,1,0.474,2.156,1.533,3.568z"></path><path fill="currentColor" d="M1.533,2.568L8,11.193V0L2.812,0C1.042,0,0.474,1.156,1.533,2.568z"></path></svg>`;
+    }
+  }
+  return "";
+};
+
+const setBorder = (e) => {
+  if (e.name != isUp) {
+    if (e.name === "Mindqube") {
+      return "rounded-tr-none";
+    } else {
+      return "rounded-tl-none";
+    }
+  }
+  return "";
 };
 
 const setBodyDate = (e) => {
@@ -819,28 +842,22 @@ const setBodyChat = (user) => {
   user.whatsapp.map((e, i) => {
     $bodyMessages.innerHTML += `
     
-    <div class="relative  rounded-lg ${setMargin(e.name)}">
+    <div class="relative break-words   rounded-lg ${setMargin(e)} ">
       ${setBodyDate(e.timestamp)}
-      <div class=" rounded-md p-[6px]  pl-2 flex gap-2 relative max-w-max ${
-        e.name === "Mindqube"
-          ? "ml-auto mr-4 bg-[#005c4b]"
-          : "bg-[#202c33]  ml-4"
-      }">
+      ${setSvgChat(e)}
+      <div class=" rounded-md ${setBorder(
+        e
+      )} p-[6px] break-words pl-2 flex gap-2 relative max-w-[65%] w-fit ${
+      e.name === "Mindqube" ? "ml-auto mr-4 bg-[#005c4b]" : "bg-[#202c33]  ml-4"
+    }">
         <div class="text-[14px] break-words">${e.message}
-          <div class="flex justify-end items-center gap-1 relative  float-right  mt-[12px] ml-[5px] mb-[-8px]">
+          <div class="flex justify-end items-center gap-1   float-right  mt-[12px] ml-[5px] mb-[-8px]">
             <div class="text-[11px] text-[#82929b] p-0 m-0">${getHours(
               e.timestamp
             )}</div>
             <div>${checked(e.status)}</div>
           </div>    
         </div>
-
-        <div class="flex justify-end items-center gap-1 opacity-0 hidden">
-          <div class="text-[12px] text-[#82929b] p-0 m-0">00:00</div>
-          <div>${checked(e.status)}</div>
-        </div>
-
-        
       </div>
     </div>`;
     isUp = e.name;
